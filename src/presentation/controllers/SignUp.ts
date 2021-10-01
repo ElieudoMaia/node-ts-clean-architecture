00f7e17fed5b30823861ee22352ed916400ad435
Elieudo Maia <elieudo.maia@g4flex.com.br>
2021-09-30 23:21:05 -0300

    refactor: make SignUp controller implements a default controllers interface

--- src/presentation/controllers/SignUp.ts
+++ src/presentation/controllers/SignUp.ts
@@ -1,10 +1,11 @@
 import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest } from '../helpers/htttp-helper'
+import { Controller } from '../protocols/controller'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 
 const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
 
-export class SignUpController {
+export class SignUpController implements Controller {
   handle (httpRequest: HttpRequest): HttpResponse {
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
