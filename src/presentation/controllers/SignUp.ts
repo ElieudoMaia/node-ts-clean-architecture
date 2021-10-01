1b8334967a7266eace6ef8ddf562e38e09f9b057
Elieudo Maia <elieudo.maia@g4flex.com.br>
2021-09-30 23:13:31 -0300

    refactor: simplify validation code

--- src/presentation/controllers/SignUp.ts
+++ src/presentation/controllers/SignUp.ts
@@ -2,12 +2,16 @@ import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest } from '../helpers/htttp-helper'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 
+const requiredFields = ['name', 'email']
+
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamError('name'))
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
     }
 
-    return badRequest(new MissingParamError('email'))
+    return badRequest(new Error())
   }
 }
