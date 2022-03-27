de553aabb29c588c0f16dfa6fb75f8cecb6f861d
Elieudo Maia <elieudo.maia@g4flex.com.br>
2022-03-27 16:36:53 -0300

    feat: ensure LogMongoRepository creates an error log on success

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -13,5 +13,5 @@ export const makeSignUpController = (): Controller => {
   const addAccount = new DbAddAccount(bcryptAdapter, addAccountRepository)
   const emailValidatorAdapter = new EmailValidatorAdapter()
   const sigupController = new SignUpController(emailValidatorAdapter, addAccount)
-  return new LogControllerDecorator(sigupController, { log: async () => {} })
+  return new LogControllerDecorator(sigupController, { logError: async () => {} })
 }
