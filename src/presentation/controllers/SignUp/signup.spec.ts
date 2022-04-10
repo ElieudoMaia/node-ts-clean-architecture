35c948fd2edd00311aa104e8b5465279fbfcba43
Elieudo Maia <elieudo.maia@g4flex.com.br>
2022-04-10 11:58:57 -0300

    test: ensure signup controller returns 400 if validation fails

--- src/presentation/controllers/SignUp/signup.spec.ts
+++ src/presentation/controllers/SignUp/signup.spec.ts
@@ -1,4 +1,5 @@
 import { InvalidParamError, MissingParamError, ServerError } from '../../errors'
+import { badRequest } from '../../helpers/htttp-helper'
 import { SignUpController } from './SignUp'
 import { AccountModel, AddAccount, AddAccountModel, EmailValidator, HttpRequest, Validation } from './signup-protocols'
 
@@ -196,5 +197,12 @@ describe('SignUp Controller', () => {
     const httpRequest = makeFakeRequestData()
     await sut.handle(httpRequest)
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
+  })
+
+  test('Should return 400 if validation returns an error', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('any_field'))
+    const httpResponse = await sut.handle(makeFakeRequestData())
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('any_field')))
   })
 })
