f65582666d8563c7452d3f499062347c557370ff
Elieudo Maia <elieudo.maia@g4flex.com.br>
2022-04-21 15:25:30 -0300

    test: ensure BcryptAdapter returns true when compare succeeds

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -42,4 +42,10 @@ describe('Bcrypt Adapter', () => {
     await sut.compare('any_value', 'any_hash')
     expect(compareSpy).toHaveBeenCalledWith('any_value', 'any_hash')
   })
+
+  test('Should return true when compare succeeds', async () => {
+    const sut = makeSut()
+    const isValid = await sut.compare('any_valid_value', 'any_valid_hash')
+    expect(isValid).toBe(true)
+  })
 })
