52a6d4a149b443bf43623e30ac294c58b432add9
Elieudo Maia <elieudo.maia@g4flex.com.br>
2022-05-31 21:49:05 -0300

    feat: ensure AddSurveyController calls usecase with correct values

---
+++ src/domain/usecases/add-survey.ts
@@ -0,0 +1,13 @@
+export interface AnswerModel {
+  answer: string
+  image: string
+}
+
+export interface AddSurveyModel {
+  question: string
+  answers: AnswerModel[]
+}
+
+export interface AddSurvey {
+  add: (survey: AddSurveyModel) => Promise<void>
+}
--- src/presentation/controllers/survey/add-survey/add-survey-controller.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -1,14 +1,18 @@
 import { badRequest } from '../../../helpers/http/htttp-helper'
-import { Controller, HttpRequest, HttpResponse, Validation } from './add-survey-protocols'
+import { AddSurvey, Controller, HttpRequest, HttpResponse, Validation } from './add-survey-protocols'
 
 export class AddSurveyController implements Controller {
   constructor (
-    private readonly validation: Validation
+    private readonly validation: Validation,
+    private readonly addSurvey: AddSurvey
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const error = this.validation.validate(httpRequest.body)
     if (error) return badRequest(error)
+
+    await this.addSurvey.add(httpRequest.body)
+
     return { body: {}, statusCode: 200 }
   }
 }
