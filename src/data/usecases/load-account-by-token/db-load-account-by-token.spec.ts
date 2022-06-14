d880ea192c888a5cfdb30ce008d693009412dcb8
Elieudo Maia <elieudo.maia@g4flex.com.br>
2022-06-13 21:23:08 -0300

    test: ensure DbLoadAccountByToken throws if LoadAccountByTokenRepository throws

--- src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
@@ -86,4 +86,11 @@ describe('DbLoadAccountByToken', () => {
     const promise = sut.load('any_token', 'any_role')
     await expect(promise).rejects.toThrow('decrypt_error')
   })
+
+  test('Should throw if LoadAccountByTokenRepository throws', async () => {
+    const { sut, loadAccountByTokenRepositoryStub } = makeSut()
+    jest.spyOn(loadAccountByTokenRepositoryStub, 'loadByToken').mockRejectedValueOnce(new Error('load_token_error'))
+    const promise = sut.load('any_token', 'any_role')
+    await expect(promise).rejects.toThrow('load_token_error')
+  })
 })
