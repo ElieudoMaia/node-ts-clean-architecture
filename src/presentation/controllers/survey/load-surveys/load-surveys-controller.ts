b97e02ca58a74373ec094c9e4a395283e976920f
Elieudo Maia <elieudo.maia@g4flex.com.br>
2022-06-25 16:22:22 -0300

    feat: ensure LoadSurveys controller throws if LoadSurveys throws

--- src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
@@ -1,11 +1,15 @@
-import { ok } from '../../../helpers/http/htttp-helper'
+import { ok, serverError } from '../../../helpers/http/htttp-helper'
 import { HttpRequest, HttpResponse } from '../add-survey/add-survey-protocols'
 import { Controller, LoadSurveys } from './load-surveys-protocols'
 
 export class LoadSurveysController implements Controller {
   constructor (private readonly loadSurveys: LoadSurveys) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const surveys = await this.loadSurveys.load()
-    return ok(surveys)
+    try {
+      const surveys = await this.loadSurveys.load()
+      return ok(surveys)
+    } catch (error) {
+      return serverError(error as Error)
+    }
   }
 }
