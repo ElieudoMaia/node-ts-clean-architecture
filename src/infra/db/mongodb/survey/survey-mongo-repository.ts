963d411d60ff260aa1f504908e3a77da18a28208
Elieudo Maia <elieudo.maia@g4flex.com.br>
2022-06-27 20:15:26 -0300

    feat: ensure SurveyMongoRepository load all surveys

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,7 +1,23 @@
+import { LoadSurveysRepository } from '../../../../data/protocols/db/survey/load-survey-repository'
 import { AddSurveyModel, AddSurveyRepository } from '../../../../data/usecases/add-survey/db-add-survey-protocols'
+import { SurveyModel } from '../../../../domain/models/survey'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class SurveyMongoRepository implements AddSurveyRepository {
+export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository {
+  async loadAll (): Promise<SurveyModel[]> {
+    const surveyCollection = await MongoHelper.getCollection('surveys')
+    const surveys = await surveyCollection.find({}).toArray()
+    const surveysDTO: SurveyModel[] = surveys.map(survey => {
+      return {
+        id: survey._id.toString(),
+        question: survey.question,
+        answers: survey.answers,
+        date: survey.date
+      }
+    })
+    return surveysDTO
+  }
+
   async add (data: AddSurveyModel): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection('surveys')
     await surveyCollection.insertOne(data)
