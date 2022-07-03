eca45c4af4874a4d5f4a29cdc4a1ce3443c34c33
Elieudo Maia <elieudo.maia@g4flex.com.br>
2022-07-03 07:14:05 -0300

    test: ensure LoadSurveys route returns 200 if a valid token was provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -69,6 +69,36 @@ describe('Survey Routes', () => {
     })
   })
 
+  test('Should return 200 on load surveys with a valid accessToken', async () => {
+    const res = await accountCollection.insertOne({
+      name: 'Fake User',
+      email: 'elieudo.maia@gmail.com',
+      password: '123'
+    })
+
+    const id = res.insertedId.toString()
+
+    const accessToken = sign({ id }, env.jwtSecret)
+
+    await accountCollection.updateOne({ _id: res.insertedId }, {
+      $set: { accessToken }
+    })
+
+    await surveyCollection.insertOne({
+      question: 'any_question',
+      answers: [
+        { image: 'any_image', answer: 'any_answer' },
+        { answer: 'answer' }
+      ],
+      date: new Date()
+    })
+
+    await request(app)
+      .get('/api/surveys')
+      .set('x-access-token', accessToken)
+      .expect(200)
+  })
+
   describe('GET /surveys', () => {
     test('Should return 403 on load survey withhout accessToken', async () => {
       await request(app)
